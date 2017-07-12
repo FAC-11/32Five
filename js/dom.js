8c17ed04b2acc3d517db116ff0926258d4d147e5
mineshmshah <mineshmshah@hotmail.co.uk>
2017-07-12 17:27:08 +0100

    added logic for travel and separated callback #11 #3 #6 #4

---
+++ js/dom.js
@@ -0,0 +1,4 @@
+document.getElementById("search-button").addEventListener('click', function(e){
+  e.preventDefault();
+  console.log(document.getElementById("post-search").value);
+})
--- js/logic.js
+++ js/logic.js
@@ -1,25 +1,5 @@
 // travel APi
-console.log("hi")
-var tflJourneyURL= "https://api.tfl.gov.uk/Journey/JourneyResults/tw9%204aa/to/e2%200sy";
-
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var tflArray = JSON.parse(xhr.responseText);
-        console.log(tflArray.journeys[0].duration);
-        console.log(tflArray.journeys[0].startDateTime);
-        console.log(tflArray.journeys[0].arrivalDateTime);
-        console.log()
-        tflArray.journeys[0].legs.forEach(function(leg){
-          console.log(leg.duration)
-          console.log(leg.mode.name)
-          console.log(leg.instruction.summary)
-        })
-
-        //console.log(tflArray.journeys[0])
-    }
-};
+var XHRFunctions={
 xhr.open("GET", tflJourneyURL, true);
 xhr.send();
 // weather api
@@ -44,8 +24,44 @@ function getWeather(postcode, callback) {
   request(url, callback);
 }
 
-window.onload = function() {
-  getWeather('E20SY', function(response) {
-    console.log(response);
-  });
+function getTravel(postcode,callback){
+  var shortPostcode = postcode.slice(0, -3);
+  var url = 'https://api.tfl.gov.uk/Journey/JourneyResults/'
+  url += shortPostcode + '/to/e2%200sy'
+  request(url,callback)
+}
+}
+
+// window.onload = function() {
+//   getWeather('E20SY', function(response) {
+//     console.log(response);
+//   });
+// };
+
+// travel APi
+/*
+console.log("hi")
+var tflJourneyURL= "https://api.tfl.gov.uk/Journey/JourneyResults/tw9%204aa/to/e2%200sy";
+
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var tflArray = JSON.parse(xhr.responseText);
+        console.log(tflArray.journeys[0].duration);
+        console.log(tflArray.journeys[0].startDateTime);
+        console.log(tflArray.journeys[0].arrivalDateTime);
+        console.log()
+        tflArray.journeys[0].legs.forEach(function(leg){
+          console.log(leg.duration)
+          console.log(leg.mode.name)
+          console.log(leg.instruction.summary)
+        })
+
+        //console.log(tflArray.journeys[0])
+    }
 };
+xhr.open("GET", tflJourneyURL, true);
+xhr.send();
+*/
+module.exports=XHRFunctions;
