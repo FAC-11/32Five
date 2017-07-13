30b93632eecb1392e0348bf30aef12ba27843d6e
mineshmshah <mineshmshah@hotmail.co.uk>
2017-07-13 10:49:00 +0100

    added first DOM attempy, containers, draft css that needs to be editted but is a working version. fixed error in logic when calling functions to dom #11 #3 #7 #6 #4

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,39 @@
 //travel changes
-document.getElementById("search-button").addEventListener('click', function(e){
-  e.preventDefault();
-  console.log(document.getElementById("post-search").value);
-})
+var summaryContainer = document.getElementById("summary-container");
+var detailsContainer = document.getElementById("details-container");
+var infoContainer = document.getElementById("map-container");
+
+
+// Add postcode and run events
+document.querySelector(".postcode__submit").addEventListener('click', function(e){
+    e.preventDefault();
+    console.log(document.querySelector(".postcode__input").value);
+    var postcode = document.querySelector(".postcode__input").value;
+    logic.getTravel(postcode, travelCallback);
+});
+
+var travelCallback = function(response){
+    var summaryNode = document.createElement('ul');
+    var summaryListNode1 = document.createElement('li');
+    var summaryListNode2 = document.createElement('li');
+    var summaryListNode3 = document.createElement('li');
+    var summarySpan1 = document.createElement('span');
+    summarySpan1.textContent = "Total duration: " + response.journeys[0].duration;
+    var summarySpan2 = document.createElement('span');
+    summarySpan2.textContent = "Start Date & Time: " + response.journeys[0].startDateTime;
+    var summarySpan3 = document.createElement('span');
+    summarySpan3.textContent = "Arrival Date & Time: " + response.journeys[0].arrivalDateTime;
+    summaryListNode1.appendChild(summarySpan1);
+    summaryListNode2.appendChild(summarySpan2);
+    summaryListNode3.appendChild(summarySpan3);
+
+    summaryNode.appendChild(summaryListNode1);
+    summaryNode.appendChild(summaryListNode2);
+    summaryNode.appendChild(summaryListNode3);
+    summaryContainer.replaceChild(summaryNode,summaryContainer.firstChild);
+    console.log(response);
+};
+
 //weather changes
 window.onload = function() {
   var postcodeSubmit = document.querySelector(".postcode__submit");
--- js/logic.js
+++ js/logic.js
@@ -1,33 +1,32 @@
 var logic = {
-request: function (url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var response = JSON.parse(xhr.responseText);
-      callback(response);
-    }
-  };
-  xhr.open('GET', url, true);
-  xhr.send();
-},
+    request: function (url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                callback(response);
+            }
+        };
+        xhr.open('GET', url, true);
+        xhr.send();
+    },
 
 //accessing weather API
-getWeather: function (postcode, callback) {
-  var shortPostcode = postcode.slice(0, -3).trim();
-  var url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-  url += shortPostcode + ',GB';
-  url += '&appid=' + weatherApiKey;
-  request(url, callback);
-},
+    getWeather: function (postcode, callback) {
+        var shortPostcode = postcode.slice(0, -3).trim();
+        var url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+        url += shortPostcode + ',GB';
+        url += '&appid=' + weatherApiKey;
+        logic.request(url, callback);
+    },
 
 
-function getTravel(postcode,callback){
-  var shortPostcode = postcode.slice(0, -3);
-  var url = 'https://api.tfl.gov.uk/Journey/JourneyResults/'
-  url += shortPostcode + '/to/e2%200sy'
-  request(url,callback)
-}
-}
+    getTravel: function(postcode,callback){
+        var url = 'https://api.tfl.gov.uk/Journey/JourneyResults/';
+        url += postcode + '/to/e2%200sy';
+        logic.request(url,callback);
+    },
+
 
 // window.onload = function() {
 //   getWeather('E20SY', function(response) {
@@ -36,34 +35,34 @@ function getTravel(postcode,callback){
 // };
 
 // travel APi
-/*
-console.log("hi")
-var tflJourneyURL= "https://api.tfl.gov.uk/Journey/JourneyResults/tw9%204aa/to/e2%200sy";
+  /*
+   console.log("hi")
+   var tflJourneyURL= "https://api.tfl.gov.uk/Journey/JourneyResults/tw9%204aa/to/e2%200sy";
 
-var xhr = new XMLHttpRequest();
+   var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var tflArray = JSON.parse(xhr.responseText);
-        console.log(tflArray.journeys[0].duration);
-        console.log(tflArray.journeys[0].startDateTime);
-        console.log(tflArray.journeys[0].arrivalDateTime);
-        console.log()
-        tflArray.journeys[0].legs.forEach(function(leg){
-          console.log(leg.duration)
-          console.log(leg.mode.name)
-          console.log(leg.instruction.summary)
-        })
+   xhr.onreadystatechange = function() {
+   if (xhr.readyState == 4 && xhr.status == 200) {
+   var tflArray = JSON.parse(xhr.responseText);
+   console.log(tflArray.journeys[0].duration);
+   console.log(tflArray.journeys[0].startDateTime);
+   console.log(tflArray.journeys[0].arrivalDateTime);
+   console.log()
+   tflArray.journeys[0].legs.forEach(function(leg){
+   console.log(leg.duration)
+   console.log(leg.mode.name)
+   console.log(leg.instruction.summary)
+   })
 
-        //console.log(tflArray.journeys[0])
+   //console.log(tflArray.journeys[0])
+   }
+   };
+   xhr.open("GET", tflJourneyURL, true);
+   xhr.send();
+   */
+    kToC: function (kelvin){
+        return  kelvin - 273.15;
     }
 };
-xhr.open("GET", tflJourneyURL, true);
-xhr.send();
-*/
-kToC: function (kelvin){
-return  kelvin - 273.15;
-}
-};
 module.exports=(logic);
-module.exports=XHRFunctions;
+
